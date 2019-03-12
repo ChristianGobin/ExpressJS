7549e8798a6c7d99ca1102ea46b0869002a002cf
Christian G <chris.gobin@proximite.space>
2019-03-12 15:33:44 -0400

    update with ES6 syntax
    added arrow functions, const, and dynamic template syntax.

--- firstReq.js
+++ firstReq.js
@@ -1,11 +1,13 @@
 //making a request through node
-var request = require('request');
-request("http://www.google.com", function(error, res, body){
+const request = require('request');
+request('https://jsonplaceholder.typicode.com/users/1', (error, res, body) => {
+	//eval(require('locus'))
 	if(error){
 		console.log("There was an error");
 	} else {
 		if(res.statusCode==200){
-			console.log(body);
+			var rawData = JSON.parse(body);
+			console.log(`${rawData.name} lives in ${rawData.address.city}`);
 		}
 	}
 });
